202c5dfe34c6604aa2177fe9ce953fc6276ed4e6
Cichy420 <srej100@gmail.com>
2022-06-10 04:41:47 +0200

    Made the applications access the mapped file rapidly to create a race condition, seems like it's more robust than I though as even the threads running as fast as possible, there were no crashes. The mutex might actually be unnecessary

--- CPPServer/CPPServer/CPPServer.cpp
+++ CPPServer/CPPServer/CPPServer.cpp
@@ -3,6 +3,7 @@
 
 #include <windows.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
 #include <tchar.h>
 
@@ -58,39 +59,7 @@ OVObject constructOVO(short type, float x, float y, short r, short g, short b, s
 
 int main()
 {
-    std::cout << "CPP Server v0.8.3" << std::endl;
-
-    std::cout << sizeof(OVObject) << std::endl;
-
-    std::cout << "the size of int is: " << sizeof(int) << std::endl;
-    std::cout << "the size of short is: " << sizeof(short) << std::endl;
-    std::cout << "the size of float is: " << sizeof(float) << std::endl;
-    std::cout << "the size of char[128] is: " << sizeof(char[128]) << std::endl;
-    std::cout << "the size of LPCTSTR is: " << sizeof(LPCTSTR) << std::endl;
-
-    int countTest = 0;
-    std::cin >> countTest;
-
-    OVObject* cinArray;
-    cinArray = new OVObject[countTest];
-    std::cout << "count is: " << countTest << " sizeof array is: " << sizeof(cinArray) << std::endl;
-
-    std::cin.get();
-    std::cin.get();
-
-    for (int i = 0; i < countTest; i++)
-    {
-        std::string ftestObjStrCin;
-        char ftestObjTxtCin[128];
-
-        ftestObjStrCin = "Cin Test object number " + std::to_string(i);
-        strcpy_s(ftestObjTxtCin, ftestObjStrCin.c_str());
-
-        cinArray[i] = constructOVO(OV_LINE, 1.1f, 1.2f, (1 + i), 2, 3, 4, 2.1f, 2.2f, 100.1f, 100.2f, ftestObjTxtCin);
-    }
-
-    std::cout << "cin last r: " << cinArray[countTest - 1].r << " | " << "cin last text: " << cinArray[countTest-1].text << std::endl;
-    std::cout << "cin first r: " << cinArray[0].r << " | " << "cin first: " << cinArray[0].text << std::endl;
+    std::cout << "CPP Server v0.8.9" << std::endl;
 
     HANDLE hMapFile;
     DWORD64 pBuf;
@@ -99,28 +68,54 @@ int main()
 
     if (hMapFile == NULL)
     {
-        _tprintf(TEXT("Could not create file mapping object (%d).\n"),
-            GetLastError());
+        _tprintf(TEXT("Could not create file mapping object (%d).\n"), GetLastError());
         return 1;
     }
+
     pBuf = (DWORD64)MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, BUF_SIZE);
 
     if (pBuf == NULL)
     {
-        _tprintf(TEXT("Could not map view of file (%d).\n"),
-            GetLastError());
-
+        _tprintf(TEXT("Could not map view of file (%d).\n"), GetLastError());
         CloseHandle(hMapFile);
-
         return 1;
     }
+    srand(static_cast <unsigned> (time(0)));
 
-    std::cout << "the size of pbuff is: " << (PVOID)pBuf << std::endl;
-    std::cout << "the size of pbuff is: " << (PVOID)(pBuf+2) << std::endl;
+    int countTest = 5;
 
+    OVObject* cinArray;
+    cinArray = new OVObject[countTest];
 
-    CopyMemory((PVOID)(pBuf), &countTest, 4);
-    CopyMemory((PVOID)(pBuf+4), cinArray, sizeof(OVObject)*countTest);
+    std::cin.get();
+    while (true)
+    {
+        //countTest = (rand() % 5) +1
+        for (int i = 0; i < countTest; i++)
+        {
+            std::string ftestObjStrCin;
+            char ftestObjTxtCin[128];
+
+            ftestObjStrCin = "TSTOBJnumber " + std::to_string(i);
+            strcpy_s(ftestObjTxtCin, ftestObjStrCin.c_str());
+
+            float randX = 0.1 + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (4321.4321)));
+            short randR = static_cast<short>(rand() % 256);
+
+            cinArray[i] = constructOVO(OV_LINE, randX, 1.2f, randR, 2, 3, 4, 2.1f, 2.2f, 100.1f, 100.2f, ftestObjTxtCin);
+        }
+
+        CopyMemory((PVOID)(pBuf), &countTest, 4);
+        CopyMemory((PVOID)(pBuf + 4), cinArray, sizeof(OVObject) * countTest);
+
+        system("cls");
+        std::cout << "count: " << countTest << std::endl;
+        for (int i = 0; i < countTest; i++)
+        {
+            std::cout << cinArray[i].text << " | x: " << cinArray[i].x << " | r: " << cinArray[i].r << std::endl;
+        }
+        //Sleep(500);
+    }
 
     std::cout << "copied" << std::endl;
     _getch();
@@ -138,3 +133,86 @@ int main()
     std::cin.get();
 
 }
+
+//int main()
+//{
+//    std::cout << "CPP Server v0.8.3" << std::endl;
+//
+//    std::cout << sizeof(OVObject) << std::endl;
+//
+//    std::cout << "the size of int is: " << sizeof(int) << std::endl;
+//    std::cout << "the size of short is: " << sizeof(short) << std::endl;
+//    std::cout << "the size of float is: " << sizeof(float) << std::endl;
+//    std::cout << "the size of char[128] is: " << sizeof(char[128]) << std::endl;
+//    std::cout << "the size of LPCTSTR is: " << sizeof(LPCTSTR) << std::endl;
+//
+//    int countTest = 0;
+//    std::cin >> countTest;
+//
+//    OVObject* cinArray;
+//    cinArray = new OVObject[countTest];
+//    std::cout << "count is: " << countTest << " sizeof array is: " << sizeof(cinArray) << std::endl;
+//
+//    std::cin.get();
+//    std::cin.get();
+//
+//    for (int i = 0; i < countTest; i++)
+//    {
+//        std::string ftestObjStrCin;
+//        char ftestObjTxtCin[128];
+//
+//        ftestObjStrCin = "Cin Test object number " + std::to_string(i);
+//        strcpy_s(ftestObjTxtCin, ftestObjStrCin.c_str());
+//
+//        cinArray[i] = constructOVO(OV_LINE, 1.1f, 1.2f, (1 + i), 2, 3, 4, 2.1f, 2.2f, 100.1f, 100.2f, ftestObjTxtCin);
+//    }
+//
+//    std::cout << "cin last r: " << cinArray[countTest - 1].r << " | " << "cin last text: " << cinArray[countTest - 1].text << std::endl;
+//    std::cout << "cin first r: " << cinArray[0].r << " | " << "cin first: " << cinArray[0].text << std::endl;
+//
+//    HANDLE hMapFile;
+//    DWORD64 pBuf;
+//
+//    hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, BUF_SIZE, szName);
+//
+//    if (hMapFile == NULL)
+//    {
+//        _tprintf(TEXT("Could not create file mapping object (%d).\n"),
+//            GetLastError());
+//        return 1;
+//    }
+//    pBuf = (DWORD64)MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, BUF_SIZE);
+//
+//    if (pBuf == NULL)
+//    {
+//        _tprintf(TEXT("Could not map view of file (%d).\n"),
+//            GetLastError());
+//
+//        CloseHandle(hMapFile);
+//
+//        return 1;
+//    }
+//
+//    std::cout << "the size of pbuff is: " << (PVOID)pBuf << std::endl;
+//    std::cout << "the size of pbuff is: " << (PVOID)(pBuf + 2) << std::endl;
+//
+//
+//    CopyMemory((PVOID)(pBuf), &countTest, 4);
+//    CopyMemory((PVOID)(pBuf + 4), cinArray, sizeof(OVObject) * countTest);
+//
+//    std::cout << "copied" << std::endl;
+//    _getch();
+//
+//    std::cout << "overinnit" << std::endl;
+//
+//    if (UnmapViewOfFile((LPCVOID)pBuf) == 0)
+//    {
+//        std::cout << "unmap failed" << std::endl;
+//    }
+//
+//    CloseHandle(hMapFile);
+//    delete[] cinArray;
+//
+//    std::cin.get();
+//
+//}
--- CSClient/CSClient/Program.cs
+++ CSClient/CSClient/Program.cs
@@ -74,12 +74,7 @@ namespace CSClient
         }
         unsafe static void Main(string[] args)
         {
-            Console.WriteLine("CSC v0.7.1");
-            Console.WriteLine("the size of short is: {0}", sizeof(short));
-            Console.WriteLine("the size of float is: {0}", sizeof(float));
-            Console.WriteLine("the size of byte is: {0}", sizeof(byte));
-            Console.WriteLine("the size of OVObject is: {0}", sizeof(OVObject));
-
+            Console.WriteLine("CSC v0.8.2");
 
             Console.ReadLine();
 
@@ -88,46 +83,29 @@ namespace CSClient
                 using (MemoryMappedViewAccessor viewAccessor = memoryMappedFile.CreateViewAccessor())
                 {
                     int count = 0;
-                    viewAccessor.Read<int>(0, out count);
-
-                    OVObject[] serverArray = new OVObject[count];
-                    viewAccessor.ReadArray<OVObject>(4, serverArray, 0, count);
-
-                    Console.WriteLine("count is: {0} | r of 0th object is: {1} | r of last object is {2}", count, serverArray[0].r, serverArray[count - 1].r);
-
-                    fixed(OVObject* temp = &serverArray[0])
+                    while(true)
                     {
-                        //byte[] tempByteArr = new byte[128];
-                        //Marshal.Copy((IntPtr)temp->text, tempByteArr, 0, 128);
-                        //Console.WriteLine();
-                        //for (int i = 0; i < 128; i++)
-                        //{
-                        //    Console.Write(" ");
-                        //    Console.Write("{0}", tempByteArr[i]);
-                        //}
-                        //Console.WriteLine();
-                        //string result = System.Text.Encoding.ASCII.GetString(tempByteArr);
-                        //Console.WriteLine("first object says: {0}", result);
+                        viewAccessor.Read<int>(0, out count);
 
-                        byte[] tempByteArr = new byte[128];
-                        tempByteArr = bytePtrToArr(temp->text, 128);
-                        Console.WriteLine();
-                        for (int i = 0; i < 128; i++)
+                        OVObject[] serverArray = new OVObject[count];
+                        viewAccessor.ReadArray<OVObject>(4, serverArray, 0, count);
+
+                        Console.Clear();
+                        Console.WriteLine("count: {0}", count);
+                        for (int i = 0; i < count; i++)
                         {
-                            Console.Write(" ");
-                            Console.Write("{0}", tempByteArr[i]);
+                            fixed (OVObject* temp = &serverArray[i])
+                            {
+                                byte[] tempByteArr = new byte[128];
+                                tempByteArr = bytePtrToArr(temp->text, 128);
+                                string result = System.Text.Encoding.ASCII.GetString(tempByteArr);
+
+                                Console.WriteLine("{0} | x: {1} | r: {2}", result, temp->x, temp->r);
+                            }
                         }
-                        Console.WriteLine();
-                        string result = System.Text.Encoding.ASCII.GetString(tempByteArr);
-                        Console.WriteLine("first object says: {0}", result);
+                        //System.Threading.Thread.Sleep(500);
                     }
-                    fixed (OVObject* temp = &serverArray[count-1])
-                    {
-                        byte[] tempByteArr = new byte[128];
-                        tempByteArr = bytePtrToArr(temp->text, 128);
-                        string result = System.Text.Encoding.ASCII.GetString(tempByteArr);
-                        Console.WriteLine("last object says: {0}", result);
-                    }
+
                 }
             }
 
@@ -135,3 +113,112 @@ namespace CSClient
         }
     }
 }
+
+//namespace CSClient
+//{
+//    unsafe struct OVObject
+//    {
+//        public short type;
+
+//        public float x;
+//        public float y;
+
+//        public short r;
+//        public short g;
+//        public short b;
+//        public short a;
+
+//        public float xTo;
+//        public float yTo;
+
+//        public float width;
+//        public float length;
+
+//        public fixed byte text[128];
+//    };
+
+//    internal class Program
+//    {
+//        const int OV_EMPTY = 0;
+//        const int OV_LINE = 1;
+//        const int OV_RECT = 2;
+//        const int OV_RECT_FILL = 3;
+//        const int OV_TEXT = 4;
+
+//        const int BUF_SIZE = 328000;
+
+//        unsafe static byte[] bytePtrToArr(byte* toConvert, int length)
+//        {
+//            byte[] result = new byte[length];
+//            bool fixFlag = false;
+
+//            for (int i = 0; i < length; i++)
+//            {
+//                if (!fixFlag)
+//                {
+//                    if (toConvert[i] == 0)
+//                    {
+//                        fixFlag = true;
+//                    }
+//                    result[i] = toConvert[i];
+//                }
+//                else
+//                {
+//                    result[i] = 0;
+//                }
+//            }
+
+//            return result;
+//        }
+//        unsafe static void Main(string[] args)
+//        {
+//            Console.WriteLine("CSC v0.7.1");
+//            Console.WriteLine("the size of short is: {0}", sizeof(short));
+//            Console.WriteLine("the size of float is: {0}", sizeof(float));
+//            Console.WriteLine("the size of byte is: {0}", sizeof(byte));
+//            Console.WriteLine("the size of OVObject is: {0}", sizeof(OVObject));
+
+
+//            Console.ReadLine();
+
+//            using (MemoryMappedFile memoryMappedFile = MemoryMappedFile.CreateOrOpen("Global\\OVTMappingObject", BUF_SIZE, MemoryMappedFileAccess.ReadWriteExecute))
+//            {
+//                using (MemoryMappedViewAccessor viewAccessor = memoryMappedFile.CreateViewAccessor())
+//                {
+//                    int count = 0;
+//                    viewAccessor.Read<int>(0, out count);
+
+//                    OVObject[] serverArray = new OVObject[count];
+//                    viewAccessor.ReadArray<OVObject>(4, serverArray, 0, count);
+
+//                    Console.WriteLine("count is: {0} | r of 0th object is: {1} | r of last object is {2}", count, serverArray[0].r, serverArray[count - 1].r);
+
+//                    fixed (OVObject* temp = &serverArray[0])
+//                    {
+
+//                        byte[] tempByteArr = new byte[128];
+//                        tempByteArr = bytePtrToArr(temp->text, 128);
+//                        Console.WriteLine();
+//                        for (int i = 0; i < 128; i++)
+//                        {
+//                            Console.Write(" ");
+//                            Console.Write("{0}", tempByteArr[i]);
+//                        }
+//                        Console.WriteLine();
+//                        string result = System.Text.Encoding.ASCII.GetString(tempByteArr);
+//                        Console.WriteLine("first object says: {0}", result);
+//                    }
+//                    fixed (OVObject* temp = &serverArray[count - 1])
+//                    {
+//                        byte[] tempByteArr = new byte[128];
+//                        tempByteArr = bytePtrToArr(temp->text, 128);
+//                        string result = System.Text.Encoding.ASCII.GetString(tempByteArr);
+//                        Console.WriteLine("last object says: {0}", result);
+//                    }
+//                }
+//            }
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
